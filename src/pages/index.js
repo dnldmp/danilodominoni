1614dc2dd13d61ad7e0b51bd644091ee8da91c15
danilo dominoni <danilodomp@gmail.com>
2020-12-15 11:57:11 -0300

    criando meu site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Foto from '../components/Foto'
+import style from '../../styles/Home.module.css'
 
 function Home(){
     return(
@@ -7,7 +8,7 @@ function Home(){
 
             <section className={style.menuSection}>
                 <div className="center">
-                    <ul class={style.menuItens}>
+                    <ul className={style.menuItens}>
                         <a href="https://bit.ly/3k6bNu1"><li>youtube</li></a>
                         <a href="https://www.instagram.com/danilodominoni/"><li>instagram</li></a>
                         <a href="#"><li>agende sua reunião</li></a>
