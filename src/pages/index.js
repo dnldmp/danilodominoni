4789470b8a01e50fa61faaf35e0b260c33b0c7f3
danilo dominoni <danilodomp@gmail.com>
2020-12-22 12:17:18 -0300

    adicionando novos itens(telegram e novo vídeo)

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,10 @@ function Home(){
             <section className={style.menuSection}>
                 <div className="center">
                     <ul className={style.menuItens}>
+                        <a href="https://youtu.be/SMi_srTBMnk"><li>novo vídeo</li></a>
                         <a href="https://bit.ly/3k6bNu1"><li>youtube</li></a>
                         <a href="https://www.instagram.com/danilodominoni/"><li>instagram</li></a>
+                        <a href="https://t.me/joinchat/KaBbHhVvoL5VbiXyHm3BAg"><li>telegram</li></a>
                         <a href="#"><li>agende sua reunião</li></a>
                     </ul>
                 </div>
