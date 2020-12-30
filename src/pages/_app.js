9e1282316ca984145ca714a311c2df8d5403204c
danilo dominoni <danilodomp@gmail.com>
2020-12-30 17:35:09 -0300

    updating sitemap and seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,18 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <title>Danilo Dominoni</title>
-                <link rel="icon" href="/favicon.ico" />
+                <meta name="author" content="Danilo Dominoni" />
+                <meta name="description" content="Danilo Dominoni - Desenvolvedor de sistemas - Programação Web e Apps." />
+                <meta name="keywords" content="desenvolvimento web,seo,programação,portifólio,web design,front-end,web developer,back-end" />
+                <meta name="robots" content="index,follow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+                <meta property="og:title" content="Danilo Dominoni"/>
+                <meta property="og:site_name" content="Danilo Dominoni"/>
+                <meta property="og:description" content="Danilo Dominoni - Desenvolvedor de sistemas - Programação Web e Apps."/>
+                <meta property="og:url" content="https://danilodominoni.com"/>
+                <meta property="og:image" content="https://danilodomiononi.com/danilodominoni.png"/>
+                <meta property="og:image:type" content="image/png"/>
+                <link rel="icon" href="https://danilodomiononi.com/favicon.ico" />
             </Head>
             <Component {...pageProps} />
         </>
