200d1e86f66a9ba15eb83cc1966dfa6b4c6512dd
danilo dominoni <danilodomp@gmail.com>
2021-01-03 12:57:55 -0300

    layout inicial do meu portifolio

--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -2,12 +2,29 @@ import style from '../../styles/Portifolio.module.css'
 
 export default function Portifolio(){
     return (
-        <div className={style.background}>
-            <style jsx global>{`
-                body {
-                    background: black;
-                }
-            `}</style>       
+        <div>
+            <section className={style.sectionAbout}>  
+                <div className={style.center}>
+                    <h2>Sobre mim</h2>
+                    <div className={style.fifityp}>
+                        <img src="/aboutme.jpg" />
+                    </div>
+                    <div className={style.fifityp}>
+                        <p>Oi, meu nome é Danilo Dominoni, sou Desenvolvedor de Sistemas desde 2014.</p>
+                        <p>Ja parou para pensar, como seria aproveitar a vida ao máximo, 
+                            sem preocupações? Hoje eu posso te proporcionar tudo isso, durante o desenvolvimento
+                            do seu projeto dos sonhos. Por este motivo, separei abaixo alguns dos meus 
+                            melhores trabalhos para você me cohecer melhor.    
+                        </p>
+                    </div>
+                    <div className="clear"></div>
+                    {/* <style jsx global>{`
+                        body {
+                            background: black;
+                        }
+                    `}</style>        */}
+                </div>
+            </section>
         </div>
     )
 }
