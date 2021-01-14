05d986274540b7b0294a700397b1bba66a722c1d
danilo dominoni <danilodomp@gmail.com>
2021-01-14 17:29:42 -0300

    update layout of portipolio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <title>Danilo Dominoni</title>
+                <link rel="icon" href="/favicon.ico"/>
                 <meta name="author" content="Danilo Dominoni" />
                 <meta name="description" content="Danilo Dominoni - Desenvolvedor de sistemas - Programação Web e Apps." />
                 <meta name="keywords" content="desenvolvimento web,seo,programação,portifólio,web design,front-end,web developer,back-end" />
@@ -17,7 +18,6 @@ function MyApp({ Component, pageProps }) {
                 <meta property="og:url" content="https://danilodominoni.com"/>
                 <meta property="og:image" content="https://danilodomiononi.com/danilodominoni.png"/>
                 <meta property="og:image:type" content="image/png"/>
-                <link rel="icon" href="https://danilodomiononi.com/favicon.ico" />
             </Head>
             <Component {...pageProps} />
         </>
