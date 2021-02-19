c309017023a07d804000a911cab59ff70368238d
Danilo Dominoni <danilodomp@gmail.com>
2021-02-19 16:44:47 -0300

    changing layout of portifolio

---
+++ src/pages/api/client.js
@@ -0,0 +1,23 @@
+import connect from '../../utils/database';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    // eslint-disable-next-line object-curly-newline
+    const { name, surname, email, phone } = req.body;
+    if (!name || !surname || !email || !phone) {
+      res.status(400).json({ error: 'Faltando campos no body' });
+      return;
+    }
+
+    const { db } = await connect();
+    const response = await db.collection('clients').insertOne({
+      name,
+      surname,
+      email,
+      phone,
+    });
+    res.status(200).json(response.ops[0]);
+  } else {
+    res.status(400).json({ error: 'Metodo errado na request' });
+  }
+};
--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -1,60 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Image from 'next/image';
+// import Link from 'next/link';
 import style from '../../styles/Portifolio.module.css';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 
-export default function Portifolio() {
-  const appresentation = `Ja parou para pensar, como seria aproveitar o seu tempo de
-    forma mais eficiente, focando nas coisas mais importantes sem preocupações? 
-    Hoje eu posso te proporcionar tudo isso, durante o desenvolvimento
-    do seu projeto. Por este motivo, separei abaixo alguns dos meus 
-    melhores trabalhos para você me cohecer melhor.`;
-  return (
-    <div>
-      <GoogleAnalytics />
-      <style jsx global>
-        {`
-          body {
-            background: black;
-          }
-        `}
-      </style>
-      <section className={style.sectionAbout}>
-        <div className={style.center}>
-          <h2>Sobre mim</h2>
-          <div className={style.fifityp}>
-            <img src="/aboutme.jpg" alt="sobre" />
-          </div>
-          <div className={style.fifityp}>
-            <p>
-              Oi, meu nome é Danilo Dominoni, estou desenvolvendo sistemas desde
-              2014.
-            </p>
-            <p>{appresentation}</p>
-          </div>
-          <a target="_blank" rel="noreferrer" href="https://bit.ly/3u4e12O">
-            <button className={style.contactUsBtn} type="button">
-              fale conosco
-            </button>
-          </a>
-        </div>
-      </section>
-      <div className="clear">
-        <section className={style.sectionProject}>
+class Portifolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // const scroll = true;
+
+  render() {
+    const registerClient = () => {
+      alert('Solicitação enviada com sucesso');
+    };
+
+    return (
+      <div>
+        <GoogleAnalytics />
+        <section id="form" className={style.sectionAbout}>
+          <Image
+            className={style.siteBackground}
+            layout="fill"
+            objectFit="cover"
+            alt="imagem de fundo site"
+            objectPosition="center"
+            src="/sitebackground.jpg"
+          />
           <div className={style.center}>
-            <img src="/background-1-site.png" alt="background" />
+            <div className={style.fifityp}>
+              <h1>
+                <p>
+                  Desenvolva seu
+                  <strong> software </strong>
+                  ou
+                  <strong> aplicativo </strong>
+                  com
+                  <strong> Danilo Dominoni</strong>
+                </p>
+                <img
+                  className={style.daniloPhoto}
+                  src="/aboutme.jpg"
+                  alt="Imagem do Danilo Dominoni"
+                />
+              </h1>
+            </div>
+            <div className={style.fifityp}>
+              <iframe
+                className={style.iframe}
+                title="stonks"
+                width="0"
+                height="0"
+                border="0"
+                name="dummyframe"
+                id="dummyframe"
+              />
+              <form
+                onSubmit={registerClient}
+                action="/api/client"
+                method="POST"
+                target="dummyframe"
+                className={style.formulario}
+              >
+                <h3>
+                  Solicite um orçamento agora e receba um roadmap do seu projeto
+                  em até 5 dias
+                </h3>
+                <input
+                  className={style.inputForm}
+                  type="text"
+                  required
+                  name="name"
+                  placeholder="Primeiro nome"
+                />
+                <input
+                  className={style.inputForm}
+                  type="text"
+                  required
+                  name="surname"
+                  placeholder="Sobrenome"
+                />
+                <input
+                  className={style.inputForm}
+                  type="text"
+                  required
+                  name="email"
+                  placeholder="seu@email.com"
+                />
+                <input
+                  className={style.inputForm}
+                  type="text"
+                  required
+                  name="phone"
+                  placeholder="(41) 99999-9999"
+                />
+                <button className={style.button} type="submit">
+                  Quero fazer um orçamento
+                </button>
+              </form>
+            </div>
           </div>
         </section>
-        <section className={style.sectionProject}>
+        {/* <div className="clear">
+        <section className={style.sectionHelp}>
           <div className={style.center}>
-            <img src="/lp1.png" alt="background" />
+            <div>
+              <img src="/danilodominoni.png" alt="imagem1" />
+              <p>danilo</p>
+              <Link href="#form" scroll={scroll}>
+                <button type="button" className={style.button}>
+                  Quero falar com os especialistas
+                </button>
+              </Link>
+            </div>
           </div>
         </section>
-        <section className={style.sectionProject}>
-          <div className={style.center}>
-            <img src="/lp2.png" alt="background" />
-          </div>
-        </section>
+      </div> */}
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Portifolio;
